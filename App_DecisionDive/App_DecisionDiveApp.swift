464a935de066bd8925f13d7f99f567832f54adc7
noura <nouraabdullahahq@gmail.com>
2023-12-25 09:52:39 +0300

    Initial Commit

---
+++ App_DecisionDive/App_DecisionDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_DecisionDiveApp.swift
+//  App_DecisionDive
+//
+//  Created by Noura Alqahtani on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_DecisionDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
